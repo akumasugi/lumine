bedbd16a46454853da57070460d5b51e7d3fd892
Ayakajanul <91958060+Ayakajanul@users.noreply.github.com>
2022-02-08 18:18:34 -0800

    Update Profile.ts

--- src/commands/General/Profile.ts
+++ src/commands/General/Profile.ts
@@ -4,7 +4,6 @@ import BaseCommand from '../../lib/BaseCommand'
 import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import Haigusha from '../Weeb/Haigusha'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
@@ -90,27 +89,12 @@ export default class Command extends BaseCommand {
                     'https://wallpaperaccess.com/full/5304840.png'
             ),
             MessageType.image,
-
-      undefined,
-
-      undefined,
-
-      `🏮 *Username: ${username}*\n\n🎗️ *About: ${
-
-        (await this.client.getStatus(user)).status || "None"
-
-      }*\n\n❤ *Haigusha: ${Haigusha}*\n\n〽️ *Level: ${level}*\n\n⭐ *Exp: ${
-
-        exp || 0
-
-      }*\n\n💫 *Role: ${role}*\n\n👑 *Admin: ${
-
-        M.groupMetadata?.admins?.includes(user) || false
-
-      }*\n\n✖ *Ban ${(await this.client.getUser(user)).ban || false}*`
-
-    );
-
-  };
-
-}
+            undefined,
+            undefined,
+            `🏮 *Username: ${username}*\n\n🎗️ *About: ${
+                (await this.client.getStatus(user)).status || 'None'
+            }*\n\n〽️ *Level: ${level}*\n\n⭐ *Exp: ${exp || 0}*\n\n💫 *Role: ${role}*\n\n👑 *Admin: ${
+                M.groupMetadata?.admins?.includes(user) || false
+            }*\n\n✖ *Ban: ${(await this.client.getUser(user)).ban || false}*`
+        )
+    }
