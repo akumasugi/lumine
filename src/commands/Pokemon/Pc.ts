c86f93f9df5e806c54b2677f984793876e079d2f
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 22:32:03 +0530

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "Lumine",
       buttons: buttons,
       headerType: 1,
     };
